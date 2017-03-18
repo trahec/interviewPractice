5ed6ec1bab5a48e6c2f3858d694c38ea4599021f
trahec <christinetrahe@gmail.com>
2017-03-18 13:39:25 +0100

    improved primeNumbers by checking for 1, as 1 cannot be prime

--- primeNumbers.js
+++ primeNumbers.js
@@ -1,16 +1,19 @@
 //list all prime numbers within a given range
 
 function isPrime(value){
-	for(var x = 2; x<value; x++){ 
-		if((value%x) === 0){
-			return false;
+	if(value!=1){
+		for(var x = 2; x<value; x++){ 
+			if((value%x) === 0){
+				return false;
+			}
 		}
+		return true;
 	}
-	return true;
+	return false;
 }
 
-var lowerRange = parseInt(10);
-var upperRange = parseInt(13);
+var lowerRange = parseInt(1);
+var upperRange = parseInt(20);
 for(var y = lowerRange; y<upperRange+1; y++){
 	var result = isPrime(y);
 	if(result === true){
