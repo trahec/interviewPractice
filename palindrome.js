38a5ce0cde62cac52972e3c032f3510536b7cf7b
trahec <christinetrahe@gmail.com>
2017-03-19 10:10:43 +0100

    added some new tests for palindrome. rewrote palindrome so it runs with tests better, no longer tested based on console output

--- palindrome.js
+++ palindrome.js
@@ -1,29 +1,20 @@
 //return whether a string is a palindrome or not
 
-var test1 = "senilefelines";
-var test2 = "abcb"
+var exports = module.exports = {};
 
-function isPalindrome(string){
-	var stringArray = string.split("")
-
-	var arrayLength = stringArray.length;
-	var iterations;
-	if(arrayLength % 2===0){
-		iterations = arrayLength;
-	}else{
-		iterations = ((arrayLength - 1) / 2);
+exports.isPalindrome = function(string) {
+    
+	var array = string.split("");
+	var arrayLength = array.length;
+	var iterations = arrayLength/2;
+	if(arrayLength%2!=0){
+		iterations=(arrayLength-1)/2;
 	}
 
-	for(var frontIndex=0, lastIndex=arrayLength; frontIndex<iterations; frontIndex++, lastIndex--){
-		//compare indices
-		if( !(stringArray[frontIndex] === stringArray[lastIndex-1]) ){
+	for(var frontIndex=0, lastIndex=arrayLength-1; frontIndex<iterations; frontIndex++, lastIndex--){
+		if(array[frontIndex] != array[lastIndex]){
 			return false;
 		}
 	}
 	return true;
 }
-
-var result1 = isPalindrome(test1);
-console.log("Is " + test1 + " a palindrome? " + result1);
-var result2 = isPalindrome(test2);
-console.log("Is " + test2 + " a palindrome? " + result2);
--- test/tests.js
+++ test/tests.js
@@ -5,16 +5,17 @@ function testUniqueElements(){
 	var setA = new Set(['1', '2', '3']);
 	var setB = new Set(['3', '4', '5']);
 	var expectedResultSet = new Set(['1', '2', '4', '5']);
-	
+
 	var differenceSet = (setA.difference(setB)).union(setB.difference(setA));
 	assert.strictEqual(expectedResultSet, expectedResultSet, "error");
+	
+	console.log("Unique elements test passed. Result:" + differenceSet);
 }
 
 testUniqueElements();
 
 var positiveNumbersScript = require('../positiveNumbers.js');
 function testPositiveNumbers(){
-
 	//test numeric array
 	var mixedNumericArray = [-1, -2, 100, 89, 3, 8, -11, 4, 7, -5, -12, 0, 99];
 	var expectedResult = [ 3, 4, 7, 8, 89, 99, 100 ];
@@ -26,5 +27,32 @@ function testPositiveNumbers(){
 	var letterArray = ['a', -2];
 	var errorObj = positiveNumbersScript.cleanseArray(letterArray);
 	assert.deepEqual(errorObj.code, 500, errorObj.message);
+	
+	console.log("Positive numbers test passed. Result:" + resultArray);
 }
 testPositiveNumbers();
+
+var palindrome = require('../palindrome.js');
+function testPalindrome(){
+	//test with odd palindrome
+	var oddPalindrome = "abcba";
+	var resultForOddPalindrome = palindrome.isPalindrome(oddPalindrome);
+	var assertMessage1 = "Not expected result: " + resultForOddPalindrome;
+	assert.ok(resultForOddPalindrome, assertMessage1);
+	console.log("Palindrome test passed for '" + oddPalindrome + "' Result: " + resultForOddPalindrome);
+
+	//test with even palindrome
+	var evenPalindrome = "abba";
+	var resultForEvenPalindrome = palindrome.isPalindrome(evenPalindrome);
+	var assertMessage2 = "Not expected result: " + resultForEvenPalindrome;
+	assert.ok(resultForEvenPalindrome, assertMessage2);
+	console.log("Palindrome test passed for '" + evenPalindrome + "' Result: " + resultForEvenPalindrome);
+	
+	//test with string that's not a palindrome
+	var notPalindrome = "abcbaa";
+	var resultForNotPalindrome = palindrome.isPalindrome(notPalindrome);
+	var assertMessage3 = "Not expected result: " + resultForNotPalindrome;
+	assert.deepEqual(resultForNotPalindrome, false, assertMessage3);
+	console.log("Palindrome test passed for '" + notPalindrome + "' Result: " + resultForNotPalindrome);
+}
+testPalindrome();
