f2a9d1bc34cd700f8841601aad1ccb9a425a0dfa
trahec <christinetrahe@gmail.com>
2017-04-03 10:20:56 +0200

    day 20 bubblesort in python

---
+++ hackerrankSubmissions/30DayChallenge1/day20_bubbleSort.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import sys
+
+class Sort:
+    'class for sorting algorithms'
+    
+    def __init__(self):
+        self
+        
+    def bubbleSort(self, array):      
+        rangeValue = len(array)-1
+        totalSwaps = 0
+        swapOccured = True      
+        while rangeValue > 0 and swapOccured:
+            swapOccured = False
+            for i in range(rangeValue):
+                if(array[i] > array[i+1]):
+                    'swap values'
+                    swapOccured = True
+                    totalSwaps = totalSwaps+1
+                    temp = array[i]
+                    array[i] = array[i+1]
+                    array[i+1] = temp
+            rangeValue = rangeValue-1; 
+        print("Array is sorted in " + str(totalSwaps) + " swaps.")
+        return array
+    
+'end Sort class'    
+ 
+n = int(input().strip())
+a = [int(a_temp) for a_temp in input().strip().split(' ')]
+
+sort = Sort()
+sortedArray = sort.bubbleSort(a)
+print("First Element: " + str(sortedArray[0]))
+print("Last Element: " + str(sortedArray[len(sortedArray)-1]))
